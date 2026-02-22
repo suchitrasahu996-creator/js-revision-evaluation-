007ffe4bc943523027e241cb3fb2767c2bbbc5c3
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-02-22 16:47:11 +0530

    adding the answer

--- js-revision-evaluation/index.js
+++ js-revision-evaluation/index.js
@@ -6,7 +6,33 @@ function createBankAccount() {
       balance += amount;
       console.log("Deposited Sucesfully", "current Balance", balance);
     },
+    withdraw (amount){
+        if(amount<=0){
+            console.log("invalid amount");
+            return;
+        }
+        if(amount >balance){
+            console.log("insufficient funds");
+            return;
+
+        }
+        balance-=amount;
+        perviousTransaction.push({type:"WITHDRWA",amount});
+        return balance;
+
+    },
+    getBalance(){
+        console.log("current Balance",balance);
+
+    }
+    getTransactionHistroy(){
+        console.log("Transaction Histroy",perviousTransactions);
+        
+    }
   };
 }
 let myAccount =createBankAccount ();
 myAccount.deposit(1000);
+myAccount.withdraw(500);
+myAccount.getBalance();
+myAccount.getTransactionHistroy();
